e777806cb9bf1d9414e83b97d29d1e98a207525c
Sfack <you@example.com>
2025-03-21 13:30:11 +0200

    fixed mobile nav

--- src/mobile_header.js
+++ src/mobile_header.js
@@ -1,26 +1,32 @@
 let bars = document.querySelector('.header__bars');
 let mobileNav = document.querySelector('.header__mobile__menu');
-let mobileLinks = document.querySelectorAll('.header__mobile__menu');
+let mobileLinks = document.querySelectorAll('.header__mobile__link');
+let resumeButton = document.querySelector('.header__mobile__resume'); 
 
 let mobileNavIsVisible = false;
 
 bars.addEventListener('click', () => {
     mobileNavIsVisible = !mobileNavIsVisible;
-
-    if(mobileNavIsVisible){
-        mobileNav.style.display = 'flex';
+    
+    if (mobileNavIsVisible) {
+        mobileNav.classList.add('active');
         document.body.style.overflowY = 'hidden';
-    }else{
-        mobileNav.style.display = 'none';
+    } else {
+        mobileNav.classList.remove('active');
         document.body.style.overflowY = 'auto';
     }
+});
 
-    mobileLinks.forEach((link) => {
-        link.addEventListener('click', () => {
-            mobileNavIsVisible = false;
-            mobileNav.style.display = 'none';
-            document.body.style.overflowY = 'auto';
-        });
+mobileLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        mobileNavIsVisible = false;
+        mobileNav.classList.remove('active');
+        document.body.style.overflowY = 'auto';
     });
+});
 
+resumeButton.addEventListener('click', () => {
+    mobileNavIsVisible = false;
+    mobileNav.classList.remove('active');
+    document.body.style.overflow = 'auto';
 });
